75b9daf9c4357db3f6009318c3dd3568389c0bda
snakeztc <zhaotiancheng.hz@gmail.com>
2016-10-26 23:59:00 -0400

    working java API

---
+++ src/main/java/AMQMessenger.java
@@ -0,0 +1,101 @@
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.log4j.Logger;
+
+import javax.jms.*;
+import java.util.UUID;
+
+
+/**
+ * A blocking version of AMQ message broker. It will send a message with a certain sendTopic
+ * and return the results in a blocking fashion
+ * Created by Tiancheng Zhao on 10/26/16.
+ */
+public class AMQMessenger {
+    static Logger logger = Logger.getLogger(AMQMessenger.class);
+    private String ip;
+    private int port;
+    private String sendTopic;
+    private String recvTopic;
+    private String subscribeName;
+    private Connection connection;
+    private Session session;
+
+    public AMQMessenger(String ip, int port, String sendTopic,
+                        String recvTopic, String clientID, String subscribeName) throws Exception
+    {
+        this.ip = ip;
+        this.port = port;
+        this.sendTopic = sendTopic;
+        this.recvTopic = recvTopic;
+        this.subscribeName = subscribeName;
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://"+this.ip+":"+this.port);
+        this.connection = connectionFactory.createConnection();
+        this.connection.setClientID(clientID);
+        this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        this.connection.start();
+    }
+
+    /**
+     * Run the instance and send the message to the given ip/port/sendTopic
+     * The thread terminates right after the message is sent
+     */
+    private void send(String textMessage, String requestId) {
+        try {
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createTopic(sendTopic+requestId);
+
+            // Create a MessageProducer from the Session to the Topic or Queue
+            MessageProducer producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            // Create a messages
+            TextMessage message = session.createTextMessage(textMessage);
+
+            // VHmsg property
+            message.setStringProperty("ELVISH_SCOPE", "DEFAULT_SCOPE");
+            message.setStringProperty("MESSAGE_PREFIX", "vr" + textMessage.split(" ", 2)[0]);
+
+            // Tell the producer to send the message
+            logger.info("Sent message: "+ textMessage + " with sendTopic: " + this.sendTopic+requestId);
+            producer.send(message);
+        }
+        catch (Exception e) {
+            logger.error(e.getStackTrace());
+        }
+    }
+    private String receive(String requestId) {
+        try {
+            // auto_ack means there is no ack. Pure async
+            javax.jms.Topic clientTopic = session.createTopic(recvTopic+requestId);
+            MessageConsumer consumer = session.createDurableSubscriber(clientTopic, subscribeName+requestId);
+            Message message = consumer.receive();
+
+            logger.debug("Get a new message");
+            String messageText = "";
+            if (message instanceof TextMessage) {
+                TextMessage txtMsg = (TextMessage) message;
+                messageText = txtMsg.getText();
+            } else if(message instanceof BytesMessage) {
+                BytesMessage btMsg = (BytesMessage) message;
+                int msgLen = (int)btMsg.getBodyLength();
+                byte [] msgBody = new byte[msgLen];
+                btMsg.readBytes(msgBody);
+                messageText = new String(msgBody, 0, msgLen);
+            }
+            logger.info("Message content is: " + messageText);
+            return messageText;
+
+        } catch (Exception e) {
+            logger.error(e);
+            return "error";
+        }
+    }
+
+    public String sendAndReceive(String message) {
+        // get a unique ID for topic
+        String requestId = UUID.randomUUID().toString();
+        this.send(message, requestId);
+        return receive(requestId);
+    }
+
+}
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
+import org.apache.activemq.usage.SystemUsage;
 import org.apache.log4j.Logger;
+
 import static spark.Spark.*;
 /**
  * Main program of Java API server.
@@ -7,19 +9,29 @@ import static spark.Spark.*;
 public class App
 {
     static Logger logger = Logger.getLogger(App.class);
-    public static int PORT = 5678;
-
+    public static int httpPort = 5678;
+    public static int amqPort = 61616;
+    public static String amqIP = "localhost";
+    
     public static void main( String[] args )
     {
-        logger.info(String.format("Server Start Running at %d", App.PORT));
-        port(App.PORT);
+        logger.info(String.format("Server Start Running at %d", App.httpPort));
 
-        post("/init", (request, response) ->
-                "Hello World: " + request.body()
-        );
+        AMQMessenger messenger;
+        try {
+            messenger = new AMQMessenger(amqIP, amqPort, "http2vh", "vh2http", "MrClue", "MrClueSub");
+        } catch (Exception e) {
+            logger.error(e);
+            return;
+        }
+
+        port(App.httpPort);
+        post("/init", (request, response) -> {
+            return messenger.sendAndReceive(request.body());
+        });
 
         post("/next", (request, response) -> {
-            return "Next here";
+            return messenger.sendAndReceive(request.body());
         });
     }
 }
